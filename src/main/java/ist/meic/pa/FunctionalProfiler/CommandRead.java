966f7a72141527b7aa2a6f913b610476609d9dfc
danielfobooss <dani.fortuna97@gmail.com>
2019-03-21 16:00:05 +0000

    updated code to count reads in constructors, added comments

--- src/main/java/ist/meic/pa/FunctionalProfiler/CommandRead.java
+++ src/main/java/ist/meic/pa/FunctionalProfiler/CommandRead.java
@@ -1,35 +1,47 @@
+/*
+ * Class extending Command, responsible for adding the read counter function to the
+ * CompileTime Class read calls and counting the reads for the class that called the read.
+ */
 package ist.meic.pa.FunctionalProfiler;
 
-import javassist.CannotCompileException;
-import javassist.CtClass;
-import javassist.CtMethod;
-import javassist.NotFoundException;
+import javassist.*;
 import javassist.expr.ExprEditor;
 import javassist.expr.FieldAccess;
 
-import javax.xml.crypto.Data;
 
 public class CommandRead  extends Command{
-    public void  execute(CtClass ctClass) throws NotFoundException, CannotCompileException {
-            final String template =
-                    "{" +
-                            " Database.addReader($0.getClass());" +
-                            " $_=$0.%s;" +
-                            "}";
-            for (CtMethod ctMethod : ctClass.getDeclaredMethods()) {
-                ctMethod.instrument(new ExprEditor() {
-                    public void edit(FieldAccess fa)
-                            throws CannotCompileException {
-                        if (fa.isReader()) {
-                            String name = fa.getFieldName();
-                            fa.replace(String.format(template,
-                                    name));
-                        }
-                    }
-                });
-            }
-        }
 
+    // Adds the code that increments the read counter for the class
+    public void  execute(CtClass ctClass) throws NotFoundException, CannotCompileException {
+        final String template =
+                "{" +
+                        " Database.addReader($0.getClass());" +
+                        " $_=$0.%s;" +
+                        "}";
+        for (CtConstructor ctConstructor : ctClass.getDeclaredConstructors()) {
+            ctConstructor.instrument(addCodeIfReader(template));
+        }
+        for (CtMethod ctMethod : ctClass.getDeclaredMethods()) {
+            ctMethod.instrument(addCodeIfReader(template));
+        }
+    }
+
+    // Returns the Expression Editor with the information if the accessed field was a read or not
+    // If it was add code, otherwise do nothing
+    public ExprEditor addCodeIfReader(String template) {
+        return new ExprEditor() {
+            public void edit(FieldAccess fa)
+                    throws CannotCompileException {
+                if (fa.isReader()) {
+                    String name = fa.getFieldName();
+                    fa.replace(String.format(template,
+                            name));
+                }
+            }
+        };
+    }
+
+    // Returns string with program's total reads
     @Override
     public  String totalText() {
         int totalreads=0;
@@ -40,6 +52,7 @@ public class CommandRead  extends Command{
         return "Total reads: "+totalreads;
     }
 
+    // Returns string with class total writes
     @Override
     public  String sumText(Class c) {
         return  " reads: "+Database.dictionary.get(c).readerCounter;
