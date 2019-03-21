67f893278a0ce54df7b7f2ce641ceca423a2d161
danielfobooss <dani.fortuna97@gmail.com>
2019-03-21 16:04:49 +0000

    Added short comment describing class purpose

--- src/main/java/ist/meic/pa/FunctionalProfiler/CommandPrint.java
+++ src/main/java/ist/meic/pa/FunctionalProfiler/CommandPrint.java
@@ -1,3 +1,7 @@
+/*
+ * Class extending Command, responsible for adding the print function to the program
+ * CompileTime Class with the main method, printing the results of the read and write counters.
+ */
 package ist.meic.pa.FunctionalProfiler;
 
 import javassist.CannotCompileException;
